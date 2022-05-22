b778d7b5e995e902cc5cff55747e9e6626f6510a
marionesc <marionescou@hotmail.fr>
2022-05-22 23:07:34 +0200

    commentaire : correction com zero_padding_mode.m

--- Projet/zero_padding_mode.m
+++ Projet/zero_padding_mode.m
@@ -40,21 +40,16 @@ function zero_padding_mode(t, y, samplingRate, yBit)
  %%%      CALCULS & CONFIGURATIONS - ZERO-PADDING MODE(PROJECT ONLY)      %%%
  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
  %%% FIRST STEP --> Creation of the original FFT and division into two parts
- % On signal sampling by zero padding mode
- % Fast Fourier Transform application
  fft_ZPori      = fft(yBit);                    % Creation of original FFT signal
  fft_yZPori_size = length(fft_ZPori);           % definition of lenght of original FFT signal
  fft_yZPori_center = floor(fft_yZPori_size/2)   % definition of median height of original FFT signal 
  
- % Creating the array containing the first half of the original FFT values
+ % creation of two arrays containing respectively the first and the second half of the original FFT values
  tab_ZP1 = fft_ZPori(  1                    : fft_yZPori_center  ) ; 
- 
- % Creating the array containing the second half of the original FFT values
  tab_ZP2 = fft_ZPori(fft_yZPori_center + 1  : fft_yZPori_size    ); 
   
-
+  
  %%% SECOND STEP --> Creation of zero's array to add and fusion for final FFT
- % Creation of an array of zeros based on the desired sampling rate
  tab_ZP0 = zeros(1,(samplingRate -1 ) * (fft_yZPori_size - 1));
  
  % Array merge -- add zero array in center
@@ -86,7 +81,7 @@ function zero_padding_mode(t, y, samplingRate, yBit)
       figureTitle = 'Amplitude of the signal as a function of time';
   end
   
-  % display of the figure with by integrating the previous configuration
+  % display of the figure by integrating the previous configuration
   close all;
   % Original signal
   plot(t, yBit, 'ob')
