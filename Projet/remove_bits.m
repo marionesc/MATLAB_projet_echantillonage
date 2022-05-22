1d70419c8210d974e5340bdac3b290f8f7681d07
marionesc <marionescou@hotmail.fr>
2022-05-22 23:11:14 +0200

    commentaire : correction com remove_bit.m

--- Projet/remove_bits.m
+++ Projet/remove_bits.m
@@ -25,44 +25,43 @@ function remove_bits(projectMode = 1)
   samplingRateMessage = "Sampling rate ";
   
   switch(projectMode)
-    %%%%%%%%%%%%%%%%%%%%%%%
-    %%%%% INTERP MODE %%%%%
-    case 1 
+    case 1  
+        %% bitshift for interp method 
          userInput = inputdlg({nRemoveMessage,samplingRateMessage},  ...
-                              "User values inputs", [1,20 ; 1,20]);
+                              "User values inputs", [1,20 ; 1,20], {2, 2});
          numberOfDelete = str2num(cell2mat(userInput(1)));
          samplingRate   = str2num(cell2mat(userInput(2)));
         
          for i = 0 : length(y); 
             yBit = bitshift(y, -numberOfDelete);    
          endfor  
-         % plot(t, yBit, 'g')   FOR DEBBUG ONLY
+         % plot(t, yBit, 'g')   FOR DEBUG ONLY
          inter_mode(t , y, yBit, samplingRate);
          
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    %%%%% ZERO-PADDING MODE %%%%%         
-    case 2      
-         userInput = inputdlg({nRemoveMessage,samplingRateMessage},  ...
-                              "User values inputs", [1,0 ; 1,20], {2,2});
-         numberOfDelete = str2num(cell2mat(userInput(1)));
-         samplingRate   = str2num(cell2mat(userInput(2)));
+       
+    case 2
+        %% bitshift for zero-padding method       
+        userInput = inputdlg({nRemoveMessage,samplingRateMessage},  ...
+                             "User values inputs", [1,0 ; 1,20], {2,2});
+        numberOfDelete = str2num(cell2mat(userInput(1)));
+        samplingRate   = str2num(cell2mat(userInput(2)));
+        
+        for i = 0 : length(y);
+           yBit = bitshift(y, -numberOfDelete);    
+        endfor
+        % plot(t, yBit, 'g')   FOR DEBUG ONLY
+        zero_padding_mode(t , y, samplingRate, yBit);
          
-         for i = 0 : length(y);
-            yBit = bitshift(y, -numberOfDelete);    
-         endfor
-         % plot(t, yBit, 'g')   FOR DEBBUG ONLY
-         zero_padding_mode(t , y, samplingRate, yBit);
-         
-    %%%%%%%%%%%%%%%%%%%%%%%
-    %%%%% LINEAR MODE %%%%%
+
     case 3 
-         userInput = inputdlg(nRemoveMessage, ...
-                              'User values inputs', [1,20],{2});
-         numberOfDelete = str2num(cell2mat(userInput(1)));
-         for i = 0 : length(y);
-            yBit = bitshift(y, -numberOfDelete);    
-         endfor
-         % plot(t, yBit, 'g')   FOR DEBBUG ONLY
-         linear_mode(1, t , y, yBit);
+        %% bitshift for linear method 
+        userInput = inputdlg(nRemoveMessage, ...
+                             'User values inputs', [1,20],{2});
+        numberOfDelete = str2num(cell2mat(userInput(1)));
+        for i = 0 : length(y);
+           yBit = bitshift(y, -numberOfDelete);    
+        endfor
+        % plot(t, yBit, 'g')   FOR DEBUG ONLY
+        linear_mode(1, t , y, yBit);
     end
 endfunction
